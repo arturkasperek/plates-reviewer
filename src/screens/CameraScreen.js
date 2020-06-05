91d69b5271c148aa67e27076622f7be203fbc35b
SzBa <48062979+SzBa@users.noreply.github.com>
2020-06-05 13:49:55 +0200

    Czarny ekran na kamerze

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -1,116 +1,114 @@
-import React, {useState, useRef} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import {RNCamera} from 'react-native-camera';
+import React, { useState, useRef, useEffect } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Camera } from "expo-camera";
+import * as Permissions from "expo-permissions";
 
-const cameraScreen = ({navigation}) => {
-  let cameraRef = useRef(null);
-  const [camType, setCamType] = useState(RNCamera.Constants.Type.back);
-  const [flash, setFlash] = useState(RNCamera.Constants.FlashMode.off);
+const cameraScreen = ({ navigation }) => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [camType, setCamType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Permissions.askAsync(
+        Permissions.CAMERA,
+        Permissions.CAMERA_ROLL
+      );
+      if (status === "granted") return setHasPermission("granted");
+    })();
+  }, []);
 
   const takePicture = async () => {
-    if (cameraRef) {
-      const options = {quality: 0.5, base64: true};
-      const data = await cameraRef.current.takePictureAsync(options);
+    if (hasPermission === "granted") {
+      const options = { quality: 0.5, base64: true };
+      const data = await hasPermission.current.takePictureAsync(options);
       console.log(data.uri);
     }
   };
 
   const flipCamera = () => {
-    if (camType === RNCamera.Constants.Type.back) {
-      setCamType(RNCamera.Constants.Type.front);
+    if (camType === Camera.Constants.Type.back) {
+      setCamType(Camera.Constants.Type.front);
     } else {
-      setCamType(RNCamera.Constants.Type.back);
+      setCamType(Camera.Constants.Type.back);
     }
   };
 
   const toggleFlash = () => {
-    if (flash === RNCamera.Constants.flashMode.off) {
-      setFlash(RNCamera.Constants.flashMode.on);
+    if (flash === Camera.Constants.flashMode.off) {
+      setFlash(Camera.Constants.flashMode.on);
     } else {
-      setFlash(RNCamera.Constants.flashMode.off);
+      setFlash(Camera.Constants.flashMode.off);
     }
   };
 
-  return (
-    <View style={styles.container}>
-      <RNCamera
-        ref={cameraRef}
-        style={styles.preview}
-        type={camType}
-        flashMode={flash}
-        androidCameraPermissionOptions={{
-          title: 'Permission to use camera',
-          message: 'We need your permission to use your camera',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }}>
-        <View style={styles.buttonsArea}>
-          <TouchableOpacity
-            onPress={() => toggleFlash()}
-            style={styles.capture}>
-            <Text style={styles.buttonText}> flash </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => takePicture()}
-            style={styles.capture}>
-            <Text style={styles.buttonText}> SNAP </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => flipCamera()} style={styles.capture}>
-            <Text style={styles.buttonText}> type </Text>
-          </TouchableOpacity>
-        </View>
-      </RNCamera>
-    </View>
-  );
+  if (hasPermission === null) {
+    return <View />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <Camera
+          style={styles.preview}
+          type={camType}
+          flashMode={flash}
+          ref={(ref) => {
+            setHasPermission(ref);
+          }}
+        >
+          <View style={styles.buttonsArea}>
+            <TouchableOpacity
+              onPress={() => toggleFlash()}
+              style={styles.capture}
+            >
+              <Text style={styles.buttonText}> flash </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => takePicture()}
+              style={styles.capture}
+            >
+              <Text style={styles.buttonText}> SNAP </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => flipCamera()}
+              style={styles.capture}
+            >
+              <Text style={styles.buttonText}> type </Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black',
+    flexDirection: "column",
+    backgroundColor: "black",
   },
   preview: {
     flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    justifyContent: "flex-end",
+    alignItems: "center",
   },
   buttonsArea: {
     flex: 0,
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
   },
   buttonText: {
     fontSize: 14,
   },
   capture: {
     flex: 0,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 5,
     padding: 15,
     paddingHorizontal: 20,
-    alignSelf: 'center',
+    alignSelf: "center",
     margin: 20,
   },
 });
-
-/*
-const ReportCar = navigation => {
-  return (
-    <View style={styles.container}>
-      <Text>Report Him!</Text>
-    </View>
-  );
-};
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#E6A92A',
-  },
-});
-*/
 
 export default cameraScreen;
