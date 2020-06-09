de62d87d762a78d91d2385beef3807f5e8b3ac7f
Wiktor Hosumbek <wiktor4444@gmail.com>
2020-06-09 15:18:21 +0200

    add connection to send data to AWS

--- src/screens/ReportCar.js
+++ src/screens/ReportCar.js
@@ -1,19 +1,80 @@
 import React, { useState } from "react";
 import get from "lodash/get";
 import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
-import {Layout} from "../components/Layout";
-import {Button} from "../components/Button";
+import { Layout } from "../components/Layout";
+import { Button } from "../components/Button";
+import * as API from "../utils/API";
+import { TextInput } from "react-native-gesture-handler";
 
 const ReportCar = ({ navigation, props, route }) => {
   const imageURI = get(route, "params.imageURI");
+  const [plateNumber, setplateNumber] = useState("none");
+  const [mediaUrl, setMediaUrl] = useState();
+  const [lat, setLat] = useState("40");
+  const [long, setLong] = useState("40");
+
+  var photo = {
+    uri: imageURI,
+    type: "image/jpeg",
+    name: "photo.jpg",
+  };
+
+  const setCarImg = async (img) => {
+    try {
+      const fetchedReport = await API.setCarImage(img);
+      if (fetchedReport) {
+        setplateNumber(fetchedReport.platesProposal);
+        setMediaUrl(fetchedReport.url);
+      }
+    } catch (err) {
+      console.error("Err: ", err);
+    }
+  };
+
+  const submitReport = async (p1, p2, p3, p4) => {
+    try {
+      const fetchedReport = await API.setReport(p1, p2, p3, p4);
+    } catch (err) {
+      console.error("Err: ", err);
+    }
+  };
 
   return (
     <Layout>
       <View style={styles.container}>
         {imageURI && <Image style={styles.image} source={{ uri: imageURI }} />}
         <Text>Report Him!</Text>
-        <Button onPress={()=>navigation.navigate("Camera")} title={'Take a photo'}/>
-        <Button onPress={()=>navigation.navigate("Photo")} title={'Select photo'}/>
+        <Button
+          onPress={() => navigation.navigate("Camera")}
+          title={"Take a photo"}
+        />
+        <Button
+          onPress={() => navigation.navigate("Photo")}
+          title={"Select photo"}
+        />
+        <Button onPress={() => setCarImg(photo)} title={"check photo"} />
+        <Text>plate proposal:</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(text) => setplateNumber(text)}
+          value={plateNumber}
+        />
+        <Text>lat:</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(text) => setLat(text)}
+          value={lat}
+        />
+        <Text>long:</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(text) => setLong(text)}
+          value={long}
+        />
+        <Button
+          onPress={() => submitReport(lat, long, mediaUrl, plateNumber)}
+          title={"submit report"}
+        />
       </View>
     </Layout>
   );
@@ -34,6 +95,11 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     paddingVertical: 10,
     marginVertical: 10,
+  },
+  textInput: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
   },
 });
 
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,53 @@
 export const getAllReport = async () => {
   try {
     const response = await fetch(
-      'http://lb-plates-reviewer-1807177165.eu-north-1.elb.amazonaws.com/report',
+      "http://lb-plates-reviewer-1807177165.eu-north-1.elb.amazonaws.com/report"
+    );
+    const json = await response.json();
+    return json;
+  } catch (err) {}
+};
+
+export const setCarImage = async (img) => {
+  s3 = new FormData();
+  s3.append("image", img);
+
+  try {
+    const response = await fetch(
+      "http://lb-plates-reviewer-1807177165.eu-north-1.elb.amazonaws.com/upload-car-image",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        body: s3,
+      }
+    );
+    const json = await response.json();
+    return json;
+  } catch (err) {}
+};
+
+export const setReport = async (lat, long, mediaURL, platesNumber) => {
+  dataReport = {
+    lat: parseInt(lat, 10),
+    long: parseInt(long, 10),
+    mediaURL: mediaURL,
+    platesNumber: platesNumber,
+  };
+
+  console.log(dataReport);
+  try {
+    const response = await fetch(
+      "http://lb-plates-reviewer-1807177165.eu-north-1.elb.amazonaws.com/report",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataReport),
+      }
     );
     const json = await response.json();
     return json;
